90369868271ee81f0dd3d59b273066dbedc0b701
isrcercas <isrcercas@gmail.com>
2025-05-07 13:19:29 +0200

    creacion de objetos spring

--- src/main/java/com/aicija/Main.java
+++ src/main/java/com/aicija/Main.java
@@ -23,11 +23,12 @@ public class Main {
 		Producto manzana = new Producto("manzana", 1.5, 100, true);
 		Producto yogurt = new Producto("yogurt", 3.5, 10, true);
 		Producto avena = new Producto("avena", 8.5, 5, true);
-
+		Producto macarrones = new Producto("macarrones", 8.5, 5, true);
 		// guardar productos
 		productoRepository.save(manzana);
 		productoRepository.save(yogurt);
 		productoRepository.save(avena);
+		productoRepository.save(macarrones);
 	}
 
 }
